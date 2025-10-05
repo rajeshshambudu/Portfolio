ba63ba97341086bfd429d6b8c81902df20930c20
rajeshshambudu <168664690+rajeshshambudu@users.noreply.github.com>
2025-10-05 23:12:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function toggleTheme() {
   document.body.classList.toggle('dark-mode');
 }
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("formMessage").textContent = "Thanks! Your message has been sent.";
+});
